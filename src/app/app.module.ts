57592bfee8d958f042f27323b6b8fef9363b7d44
Thom9521 <Thom9521@edu.easj.dk>
2020-03-19 12:55:48 +0100

    Access basis component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StepperComponent } from './components/stepper/stepper.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StepperComponent } from "./components/stepper/stepper.component";
 
-import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { PersonalinformationComponent } from './components/stepper/steps/personalinformation/personalinformation.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import {MatInputModule} from '@angular/material';
-
-
-
+import { MatCardModule } from "@angular/material/card";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { ReactiveFormsModule } from "@angular/forms";
+import { PersonalinformationComponent } from "./components/stepper/steps/personalinformation/personalinformation.component";
+import { AccessbasisComponent } from "./components/stepper/steps/accessbasis/accessbasis.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     StepperComponent,
-    PersonalinformationComponent
+    PersonalinformationComponent,
+    AccessbasisComponent
   ],
   imports: [
     BrowserModule,
@@ -30,11 +30,12 @@ import {MatInputModule} from '@angular/material';
     MatCardModule,
     MatStepperModule,
     MatFormFieldModule,
-    ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule,
     MatGridListModule,
-    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
