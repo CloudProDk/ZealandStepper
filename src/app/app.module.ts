4e2441ef1d0b81cbd2476d244f0049b6ed72404a
PatrickOerum@icloud.com <patrickoerum@icloud.com>
2020-03-19 09:56:50 +0100

    Added stepper and validated component stepper concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StepperComponent } from './components/stepper/stepper.component';
 
 import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PersonalinformationComponent } from './components/stepper/steps/personalinformation/personalinformation.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StepperComponent
+    StepperComponent,
+    PersonalinformationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/stepper/steps/personalinformation/personalinformation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personalinformation',
+  templateUrl: './personalinformation.component.html',
+  styleUrls: ['./personalinformation.component.scss']
+})
+export class PersonalinformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
