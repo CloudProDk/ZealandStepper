e683f92853d4251d65a43c539c83685b5d31553f
Thom9521 <Thom9521@edu.easj.dk>
2020-03-19 16:09:43 +0100

    Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,5 @@
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StepperComponent } from './components/stepper/stepper.component';
-
-import {MatCardModule} from '@angular/material/card';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { PersonalinformationComponent } from './components/stepper/steps/personalinformation/personalinformation.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import {MatInputModule} from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
